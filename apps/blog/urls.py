02fc247e1324df8cce3d833d8c99e7f971653325
Ruslan <sounditbox@gmail.com>
2026-06-04 19:44:13 +0400

    basic cbvs

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,12 +4,12 @@ from .views import *
 app_name = 'apps.blog'
 
 urlpatterns = [
-    path('my_first_view/', my_first_view),
-    path('my_dynamic_url/<path:conv>/', dynamic_view),
-    path('vars/<int:var1>/second/<str:var2>/', two_vars),
-    path('request_review/', request_review, name='request_review'),
 
-    path('posts/', post_list, name='post_list'),
-    path('posts/<int:post_id>/', get_post, name='post_detail'),
-    path('posts/<int:post_id>/comments/', get_post_comments, name='comments'),
+    # path('posts/', post_list, name='post_list'), # FBV
+    path('posts/', PostListView.as_view(), name='post_list'),  # CBV аналог
+
+    # path('posts/<int:post_id>/', get_post, name='post_detail'),
+    path('posts/<int:post_id>/', PostDetailView.as_view(), name='post_detail'),
+    # path('posts/<int:post_id>/comments/', get_post_comments, name='comments'),
+    path('posts/<int:post_id>/comments/', CommentListView.as_view(), name='comments'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,25 +1,9 @@
 from typing import Any
 
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-from django.urls import reverse
-from django.views.decorators.csrf import csrf_exempt
-
-
-# FBV - Function Based View
-def my_first_view(request: HttpRequest) -> HttpResponse:
-    return render(request, 'first_view.html')
-
-
-def dynamic_view(request: HttpRequest, conv: str) -> HttpResponse:
-    # запрос к бд - вытащить отдельную статью
-    # сформировать контекст для html
-    return HttpResponse(f'<h1>Counter {conv}</h1>')
-
-
-def two_vars(request: HttpRequest, var1: int, var2: int) -> HttpResponse:
-    return HttpResponse(f'<h1>Data from url: 1:{var1}, 2:{var2}</h1>')
-
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import TemplateView
 
 pseudo_db = {
     'posts': {
@@ -35,80 +19,116 @@ pseudo_db = {
 }
 
 
-def get_post(request: HttpRequest, post_id: int) -> HttpResponse:
-    if post_id not in pseudo_db['posts']:
-        return render(request, '404.html', {'instance': f'Post {post_id}'})
-    post = pseudo_db['posts'][post_id]
-
-    context = {
-        'post': post,
-        'title': f'Post {post["title"]}'
-    }
-    return render(request, 'post_detail.html', context)
-
-
-def get_post_comments(request, post_id):
-    if post_id not in pseudo_db['posts']:
-        return render(request, '404.html', {'instance': f'Post {post_id}'})
-    comments = [c for c in pseudo_db['comments'].values() if
-                c['post_id'] == post_id]
-    context = {
-        'comments': comments,
-        'post_id': post_id,
-        'title': f'Comments for post {post_id}'
-    }
-
-    return render(request, 'comment_list.html', context)
+# def get_post(request: HttpRequest, post_id: int) -> HttpResponse:
+#     if post_id not in pseudo_db['posts']:
+#         return render(request, '404.html', {'instance': f'Post {post_id}'})
+#     post = pseudo_db['posts'][post_id]
+#
+#     context = {
+#         'post': post,
+#         'title': f'Post {post["title"]}'
+#     }
+#     return render(request, 'post_detail.html', context)
 
 
-def post_list(request: HttpRequest) -> HttpResponse:
-    posts = [p for p in pseudo_db['posts'].values()]
-    context: dict[str, Any] = {
-        'title': 'All posts',
-        'posts': posts,
-    }
-    return render(request, 'post_list.html', context)
+class PostDetailView(View):
+    def get(self, request, post_id):
+        if post_id not in pseudo_db['posts']:
+            return render(request, '404.html', {'instance': f'Post {post_id}'})
+        post = pseudo_db['posts'][post_id]
+
+        context = {
+            'post': post,
+            'title': f'Post {post["title"]}'
+        }
+        return render(request, 'post_detail.html', context)
 
 
-@csrf_exempt
-def request_review(request: HttpRequest) -> HttpResponse:
-    response = '<h1>Request Data:</h1>'
-    response += f'<p>Method: {request.method}</p>'
-    response += f'<p>Path: {request.path}</p>'
 
-    # response += f'<h2>Headers:</h2>'
-    # response += f'{request.headers}</p>'
-    #
-    # response += f'<h2>Body:</h2>'
-    # response += f'<p>{request.body}</p>'
+# def get_post_comments(request, post_id):
+#     if request.method == 'POST':
+#         content = request.POST.get('content')
+#         new_comment = {'post_id': post_id, 'content': content}
+#         pseudo_db['comments'][len(pseudo_db['comments']) + 1] = new_comment
+#         return redirect('blog:comments', post_id=post_id)
+#
+#     if post_id not in pseudo_db['posts']:
+#         return render(request, '404.html', {'instance': f'Post {post_id}'})
+#     comments = [c for c in pseudo_db['comments'].values() if
+#                 c['post_id'] == post_id]
+#     context = {
+#         'comments': comments,
+#         'post_id': post_id,
+#         'title': f'Comments for post {post_id}'
+#     }
+#
+#     return render(request, 'comment_list.html', context)
 
-    # response += f'<h2>Cookies:</h2>'
-    # response += f'<p>{request.COOKIES}</p>'
 
-    # Параметры из url
-    response += '<h2>Query Params:</h2>'
-    for p in request.GET:
-        response += f'<p>{p}: {request.GET[p]}</p>'
+class CommentListView(View):
+    def get(self, request, post_id):
+        if post_id not in pseudo_db['posts']:
+            return render(request, '404.html', {'instance': f'Post {post_id}'})
+        comments = [c for c in pseudo_db['comments'].values() if
+                    c['post_id'] == post_id]
+        context = {
+            'comments': comments,
+            'post_id': post_id,
+            'title': f'Comments for post {post_id}'
+        }
 
-    # Данные из формы
-    if request.POST:
-        response += f'<h2>Post data:</h2>'
-        for p in request.POST:
-            response += f'<p>{p}: {request.POST[p]}</p>'
+        return render(request, 'comment_list.html', context)
 
-    # Если в форме присылался файл
-    if request.FILES:
-        response += f'<h2>Files attached:</h2>'
+    def post(self, request, post_id):
+        content = request.POST.get('content')
+        new_comment = {'post_id': post_id, 'content': content}
+        pseudo_db['comments'][len(pseudo_db['comments']) + 1] = new_comment
+        return self.get(request, post_id)
 
-        for f in request.FILES:
-            response += f'<p>{f}: {request.FILES[f].read()}</p>'
 
-        with open('file.jpg', 'wb') as f:
-            f.write(request.FILES['file'].read())
+# def post_list(request: HttpRequest) -> HttpResponse:
+#     if request.method == 'POST':
+#         title = request.POST.get('title')
+#         content = request.POST.get('content')
+#         new_post = {'id': len(pseudo_db['posts']) + 1, 'title': title,
+#                     'content': content}
+#         pseudo_db['posts'][new_post['id']] = new_post
+#     posts = [p for p in pseudo_db['posts'].values()]
+#     context: dict[str, Any] = {
+#         'title': 'All posts',
+#         'posts': posts,
+#     }
+#     return render(request, 'post_list.html', context)
 
-    if request.user.is_authenticated:
-        response += f'<h2>User: {request.user}'
-    else:
-        response += f'<h2>Not logged in</h2>'
 
-    return HttpResponse(response)
+class PostListView(View):
+    def get(self, request: HttpRequest):
+        posts = [p for p in pseudo_db['posts'].values()]
+        context: dict[str, Any] = {
+            'title': 'All posts',
+            'posts': posts,
+        }
+        return render(self.request, 'post_list.html', context)
+
+    def post(self, request):
+        title = self.request.POST.get('title')
+        content = self.request.POST.get('content')
+        new_post = {'id': len(pseudo_db['posts']) + 1, 'title': title,
+                    'content': content}
+        pseudo_db['posts'][new_post['id']] = new_post
+        return self.get(request)
+
+#
+# class PostListView(TemplateView):
+#     template_name = 'post_list.html'
+#     extra_context = {
+#         'title': 'All posts',
+#         'posts': [p for p in pseudo_db['posts'].values()]
+#     }
+#
+#     def post(self, request):
+#         title = self.request.POST.get('title')
+#         content = self.request.POST.get('content')
+#         new_post = {'id': len(pseudo_db['posts']) + 1, 'title': title, 'content': content}
+#         pseudo_db['posts'][new_post['id']] = new_post
+#         return redirect('blog:post_list')
