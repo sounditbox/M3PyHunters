50a05f9f2e4b6c004fd4ee30e4b817810fbf6453
Ruslan <sounditbox@gmail.com>
2026-08-10 19:36:44 +0400

    PostViewset added

--- apps/blog/api.py
+++ apps/blog/api.py
@@ -1,13 +1,19 @@
+from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework.decorators import action
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, \
+    IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.routers import DefaultRouter
+from rest_framework.viewsets import ModelViewSet
 
 from apps.blog.filters import PostFilter
 from apps.blog.models import Post
 from apps.blog.permissions import IsAuthorOrReadOnly
 from apps.blog.serializers import FullPostSerializer, ShortPostSerializer, \
-    CreatePostSerializer, PartialUpdatePostSerializer
+    CreatePostSerializer, PartialUpdatePostSerializer, CommentSerializer
 
 
 class PostListApiView(ListCreateAPIView):
@@ -42,3 +48,53 @@ class PostDetailApiView(RetrieveUpdateDestroyAPIView):
         if self.request.method == 'PATCH':
             return PartialUpdatePostSerializer
         return FullPostSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.select_related('author').prefetch_related(
+        'tags')
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter]
+
+    filterset_class = PostFilter
+    search_fields = ['title', 'content', '=author__username',
+                     '^author__email']
+    ordering_fields = ['title', 'created_at', 'updated_at', 'likes', 'views']
+    ordering = ['-created_at']
+    lookup_url_kwarg = 'post_id'
+
+    def get_queryset(self):
+        q = self.queryset
+        if self.action not in ['list', 'create']:
+            q = self.queryset.prefetch_related('comments__author')
+        return q
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return CreatePostSerializer
+        return ShortPostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['get'])
+    def comments(self, *args, **kwargs):
+        return Response(
+            CommentSerializer(self.get_object().comments.all(), many=True).data)
+
+    @action(methods=['get'], detail=False)
+    def summary(self, *args, **kwargs):
+        return Response(
+            {
+                'authors': self.get_queryset().values('author').annotate(
+                    Count('author')),
+                'posts': self.get_queryset().count(),
+                'comments': self.get_queryset().values(
+                    'comments__author').annotate(Count('comments__author')),
+                'tags': self.get_queryset().values('tags').annotate(
+                    Count('tags')),
+            })
+
+
+post_router = DefaultRouter()
+post_router.register('posts', PostViewSet)
--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 import apps
-from apps.blog.api import PostListApiView, PostDetailApiView
+from apps.blog.api import PostListApiView, PostDetailApiView, post_router
 from config import settings
 
 urlpatterns = [
@@ -19,9 +19,11 @@ urlpatterns = [
     path('api/auth-token/', obtain_auth_token, name='api_auth_token'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/posts/', PostListApiView.as_view(), name='post_list_api'),
-    path('api/posts/<int:post_id>', PostDetailApiView.as_view(), name='post_detail_api'),
-              ] + debug_toolbar_urls()
+    # path('api/posts/', PostListApiView.as_view(), name='post_list_api'),
+    # path('api/posts/<int:post_id>', PostDetailApiView.as_view(), name='post_detail_api'),
+    #
+    path('api/', include(post_router.urls))
+] + debug_toolbar_urls()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
